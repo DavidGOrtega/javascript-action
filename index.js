5811d78a898222aa85608bf47640443fc3cd4fe1
davidgortega <g.ortega.david@gmail.com>
2019-12-30 23:11:55 +0100

    no wait

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const fs = require('fs');
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+
+const imgur = require('imgur');
+imgur.setClientId('9ae2688f25fae09');
+
+const github_token = core.getInput('github_token');
+const dvc_repro_file = core.getInput('dvc_repro_file');
+const dvc_repro_skip = core.getInput('dvc_repro_skip') === 'true';
+const skip_ci = core.getInput('skip_ci');
+
+const GITHUB_SHA = process.env.GITHUB_SHA;
+const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
+
+const STUB = process.env.STUB === 'true';
+
+const [owner, repo] = GITHUB_REPOSITORY.split('/');
+const octokit = new github.GitHub(github_token);
+
 
 // most @actions toolkit packages have async methods
 async function run() {
